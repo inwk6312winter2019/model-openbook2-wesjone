b8b1df3646e7bb31fb6ba2124c7b80758557bd4b
[wesjone] <[jn298501@dal.ca]>
2019-02-06 14:54:09 -0800

    still try

--- task1.4.py
+++ task1.4.py
@@ -1,15 +1,19 @@
 fin = open('Street_Centrelines.csv')
 def street_class():
-  d = {} 
+  d = {}
+  t = 0 
   class_list = []
+  class_list2 = []
   for line in fin:
     line = line.split(',')
     class_list.append(line[10])
-    for i in class_list:
-      if i not in d:
-        d[i]=list(line[2])
-      else:
-        d[i].append(list(line[2]))
+    class_list2.append(line[2])
+  for i in class_list:
+    if i not in d:
+      d[i] =list( class_list2[t])
+    else:
+      d[i].append(list(class_list2[t]))
+  t=t+1
   print(d)
 street_class()
 
