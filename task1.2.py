d9d3bbae5b06a48497938fe99adb20a303e145aa
[wesjone] <[jn298501@dal.ca]>
2019-02-06 13:32:16 -0800

    task1.2

---
+++ task1.2.py
@@ -0,0 +1,14 @@
+def count_maintenance():
+  fin = open('Street_Centrelines.csv')
+  d = {}
+  l = []
+  for line in fin:
+    line = line.split(',')  # put all required street into l
+    l.append(line[12])
+  for i in l:
+    if i not in d:    #count numbers it occurs
+      d[i]=1
+    else:
+      d[i]+=1
+  print(d)
+count_maintenance()
